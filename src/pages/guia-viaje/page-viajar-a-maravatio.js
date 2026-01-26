dfd7d2e49254ebfa160a1dd31f625455fee06d95
Jose Leonardo Gonzalez Valadez <jleonardo18071999@gmail.com>
2026-01-26 13:58:36 -0600

    add import to modal doters & travelpass modal

--- src/pages/guia-viaje/page-viajar-a-maravatio.js
+++ src/pages/guia-viaje/page-viajar-a-maravatio.js
@@ -13,6 +13,8 @@ import "../../js/slick.js?v=1.0.0";
 class PageViajarAMaravatio extends HTMLElement {
   async connectedCallback() {
     this.innerHTML = `
+    <app-modal-travelpass></app-modal-travelpass>
+      <app-modal-doters></app-modal-doters>
       <app-cotiza></app-cotiza>
 
 <!-- Components for page -->
